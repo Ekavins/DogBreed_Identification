06ba8531d79d08c73f7fa8409365bc4db69fc466
Kyubey <34502111+wonggw@users.noreply.github.com>
2018-02-05 22:07:20 +0800

    Add files via upload

--- test2.py
+++ test2.py
@@ -6,38 +6,35 @@ import cv2
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
-class data_reader():
-	def __init__(self):
-		print('Reading data...')
-		#img1 = []
-		data = []
-		label = []
-		code = []
-		df = pd.read_csv('labels.csv')
-		df1 = pd.read_csv('breeds.csv')
-		label = df1['id'].as_matrix()
-		code=LabelEncoder().fit_transform(df['breed'])
-		#print(label)
-		i=0
-		for index, row in df.iterrows():
-			image_path = 'train/'+row['id']+'.jpg'
-			img = cv2.imread(image_path)
-			#print(image_path)
-			img = cv2.resize(img,(256,256))
-			#print(img)
-			#cv2.imshow('image',img)
-			#cv2.waitKey(0)
-			#label=LabelEncoder().fit_transform(df['breed'])
-			label= code[i]
-			i= i++1
-			data_row = [img, label]
-			data.append(data_row)
+def data_reader():
+    #def __init__(self):
+       print('Reading data...')
+       data = []
+       code = []
+       df = pd.read_csv('labels.csv')
+       code = LabelEncoder().fit_transform(df['breed'])
+       #print(label)
+       i=0
+       for index, row in df.iterrows():
+           image_path = 'train/'+row['id']+'.jpg'
+           img = cv2.imread(image_path)
+           #print(image_path)
+           img = cv2.resize(img,(256,256))
+           #print(img)
+           #cv2.imshow('image',img)
+           #cv2.waitKey(0)
+           label= code[i]
+           i= i++1
+           data_row = [img,label]
+           data.append(data_row)
+        #    data = (img1,label)
+        #print (data)
+        #self.data = data
+       return data
 
-		self.data = data
-
-	def next_train_batch(self,bsize):
-		batch = random.sample(self.data,bsize)
-		return batch
+#    def next_train_batch(self,bsize):
+#        batch = random.sample(self.data,bsize)
+#        return batch
 
 
 def main_structure(inp):
@@ -46,16 +43,14 @@ def main_structure(inp):
 		inp = tf.scan(lambda _,y:tf.image.random_contrast(y,0.5,2),inp,initializer=tf.constant(0.0,shape=[256,256,3]))
 		inp = tf.scan(lambda _,y:tf.image.random_saturation(y,0.5,2),inp,initializer=tf.constant(0.0,shape=[256,256,3]))
 		mod = M.Model(inp,[None,256,256,3])
-		mod.convLayer(5,16,activation=M.PARAM_LRELU)#128_2x2
-		mod.maxpoolLayer(2) #64
-		mod.convLayer(3,32,activation=M.PARAM_LRELU)#32_4x4
-		mod.maxpoolLayer(2) # 16
-		mod.convLayer(3,64,activation=M.PARAM_LRELU)#8_8x8
-		mod.convLayer(3,128,activation=M.PARAM_LRELU)#8_8x8
-		mod.convLayer(3,128,activation=M.PARAM_LRELU)#8_8x8
-		mod.convLayer(3,256,activation=M.PARAM_LRELU)#8_8x8
+		mod.convLayer(5,16,stride=2,activation=M.PARAM_LRELU)#128_2x2
+		mod.convLayer(4,32,stride=2,activation=M.PARAM_LRELU)#64_4x4
+		mod.convLayer(3,64,stride=2,activation=M.PARAM_LRELU)#32_8x8
+		mod.convLayer(3,128,activation=M.PARAM_LRELU)#32_8x8
+		mod.convLayer(3,256,activation=M.PARAM_LRELU)
 		mod.flatten()
-		mod.fcLayer(120)
+		mod.fcLayer(256,activation=M.PARAM_RELU)
+		mod.fcLayer(1)
 		return mod.get_current_layer()
 
 
@@ -63,20 +58,20 @@ def build_graph():
 	with tf.name_scope('img_holder'):
 		img_holder = tf.placeholder(tf.float32,[None,256,256,3])
 	with tf.name_scope('lab_holder'):
-		lab_holder = tf.placeholder(tf.int32,[None])
+		lab_holder = tf.placeholder(tf.float32,[None,1])
 
 	last_layer = main_structure(img_holder)
 
 	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=lab_holder,logits=last_layer))
 	accuracy = M.accuracy(last_layer,tf.argmax(lab_holder,1))
-	train_step = tf.train.AdamOptimizer(0.01).minimize(loss)
+	train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
 	return img_holder,lab_holder,loss,train_step,accuracy,last_layer
 
 MAXITER = 100000000
-BSIZE = 32
+#BSIZE = 16
 
-reader = data_reader()
+data = data_reader()
 img_holder,lab_holder,loss,train_step,accuracy,last_layer = build_graph()
 
 with tf.Session() as sess:
@@ -86,11 +81,15 @@ with tf.Session() as sess:
 	print('Reading finish')
 	for iteration in range(MAXITER):
 		# print(iteration)
-		train_batch = reader.next_train_batch(BSIZE)
+		#train_batch = reader.next_train_batch(BSIZE)
 		# print(iteration)
-		img_batch = [i[0] for i in train_batch]
-		img_batch = np.float32(img_batch)
-		lab_batch = [i[1] for i in train_batch]
+		sample = []
+		j=random.randint(0, 10222)
+		sample = data[j]
+		img_batch = np.reshape(sample[0],[1,256,256,3])
+		lab_batch = np.reshape(sample[1],[1,1])
+		tf.cast(sample[0], tf.float32)
+		tf.cast(sample[1], tf.float32)
 		feeddict = {img_holder:img_batch, lab_holder:lab_batch}
 		# print(iteration)
 		_,acc,ls = sess.run([train_step,accuracy,loss],feed_dict=feeddict)
@@ -98,7 +97,7 @@ with tf.Session() as sess:
 		if iteration%10==0:
 			print('Iter:',iteration,'\tLoss_b:',ls,'\tAcc:',acc)
 			# print(c.max())
-			img = img_batch[0].astype(np.uint8)
+			#img = img_batch[0].astype(np.uint8)
 
 		if iteration%5000==0 and iteration!=0:
 			saver.save(sess,'./model/'+str(iteration)+'.ckpt')
